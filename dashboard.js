a2dd9693606a6342f13c394ee3bddcf3c62b3d88
Jarold Wong <jaroldw@gmail.com>
2017-03-23 15:29:05 -0700

    Add geolocation API request

--- dashboard.js
+++ dashboard.js
@@ -11,4 +11,33 @@ function updateClock() {
     clockDisplay.innerHTML = hours + ":" + minutes;
 }
 
-window.onload = updateClock;
+function getWeather() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        
+        var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&key=" + mapsAPIKey;
+        
+        // JSON request
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        
+        request.onload = function() {
+            if (request.readyState == 4 && request.status == 200) {
+                var data = JSON.parse(request.responseText);
+                var city = data["results"][2]["address_components"][1]["long_name"]
+                
+                document.getElementById("weather").innerHTML = city;
+            } else {
+                document.getElementById("weather").innerHTML = "Location Error";
+            }
+        }
+
+        request.send();
+    });
+}
+
+window.onload = function() {
+    updateClock();
+    getWeather();
+}
