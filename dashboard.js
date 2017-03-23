451ca3e7391e9005a6e883669c6959b242a89b3c
Jarold Wong <jaroldw@gmail.com>
2017-03-23 16:21:21 -0700

    Update to get weather info from yahoo api

--- dashboard.js
+++ dashboard.js
@@ -15,9 +15,9 @@ function getWeather() {
     navigator.geolocation.getCurrentPosition(function(position) {
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
-        
-        var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&key=" + mapsAPIKey;
-        
+
+        var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(SELECT%20woeid%20FROM%20geo.places%20WHERE%20text%3D%22(" + latitude + "%2C" + longitude + ")%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
         // JSON request
         var request = new XMLHttpRequest();
         request.open('GET', url, true);
@@ -25,9 +25,13 @@ function getWeather() {
         request.onload = function() {
             if (request.readyState == 4 && request.status == 200) {
                 var data = JSON.parse(request.responseText);
-                var city = data["results"][2]["address_components"][1]["long_name"]
-                
-                document.getElementById("weather").innerHTML = city;
+
+                var city = data["query"]["results"]["channel"]["location"]["city"];
+                var temp = data["query"]["results"]["channel"]["item"]["condition"]["temp"];
+                var condition = data["query"]["results"]["channel"]["item"]["condition"]["text"];
+
+                var weatherString = temp + "&deg;" + "<br>" + city;
+                document.getElementById("weather").innerHTML = weatherString;
             } else {
                 document.getElementById("weather").innerHTML = "Location Error";
             }
