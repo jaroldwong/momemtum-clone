a4a191d00cc475651e21807d49f4779601648883
Jarold Wong <jaroldw@gmail.com>
2017-03-23 10:57:49 -0700

    Add center clock display and greeting

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,14 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.create({'url': chrome.extension.getURL('dashboard.html')});
 });
+
+var clockDisplay = document.getElementById('clock');
+function updateClock() {
+    var d = new Date(),
+        hours = d.getHours(),
+        minutes = d.getMinutes();
+
+    clockDisplay.innerHTML = hours + ":" + minutes;
+}
+
+window.onload = updateClock;
